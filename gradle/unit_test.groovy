364f8d08a58aaa3e17cd35f3f6146d10ea917f69
xiangxu <iasonliu@gmail.com>
2019-08-07 11:37:33 +0800

    [xiangxu] add build and test

--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,4 +1,9 @@
 void call(){
-  println "gradle: unit_test()"
-  sh 'docker run --rm -u gradle -v ${env.WORKSPACE}:/app -w /app  sisglusdevops/gradle:4.10.3 gradle test'
+
+  steps {
+      println "gradle: unit_test()"
+      echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+      sh 'docker run --rm -u gradle -v ${env.WORKSPACE}:/app -w /app  sisglusdevops/gradle:4.10.3 gradle test'
+   }
+
 }
