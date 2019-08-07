228384510fb81997bb7b7789ff0e56abdb5fbe74
xiangxu <iasonliu@gmail.com>
2019-08-07 14:24:16 +0800

    [xiangxu] remove stages

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -3,6 +3,7 @@ void call(){
       println "gradle: build()"
       sh 'mkdir -p /tmp/gradle-caches/${env.JOB_BASE_NAME}'
       sh 'docker run --rm  -u gradle -v /tmp/gradle-caches/${env.JOB_BASE_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle clean build'
+      junit 'build/reports/**/*.xml'
       archiveArtifacts artifacts: 'build/libs/**/*.jar', fingerprint: true
     }
 }
