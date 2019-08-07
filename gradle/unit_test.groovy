fbd95c7e2be44103d3c6073d8f7db517b65d1175
xiangxu <iasonliu@gmail.com>
2019-08-07 12:03:07 +0800

    [xiangxu] add fix test stage

--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,8 +1,8 @@
 void call(){
   stage('Testing') {
-        node{
-        println "gradle: unit_test()"
-        sh 'docker run -rm  -u gradle -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle test'
-        }
+    node{
+      println "gradle: unit_test()"
+      sh 'docker run --rm  -u gradle -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle test'
+    }
   }
 }
