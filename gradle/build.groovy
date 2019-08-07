be08750a432f90314e7401edaf978d711c263ef6
xiangxu <iasonliu@gmail.com>
2019-08-07 12:04:11 +0800

    [xiangxu] add fix buil stage

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -1,3 +1,8 @@
 void call(){
-    println "gradle: build()"
+  stage('Gradle Build') {
+    node{
+      println "gradle: build()"
+      sh 'docker run --rm  -u gradle -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle clean build'
+    }
+  }
 }
