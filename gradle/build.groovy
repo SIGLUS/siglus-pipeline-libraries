1203707f1f020a3a6db423314059441a053043e4
xiangxu <iasonliu@gmail.com>
2019-08-07 11:51:41 +0800

    [xiangxu] add remove steps

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -1,6 +1,3 @@
 void call(){
     println "gradle: build()"
-    steps {
-        sh 'docker run --rm -u gradle -v "$PWD":/app -w /app  sisglusdevops/gradle:4.10.3 gradle clean build'
-    }
 }
--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,7 +1,7 @@
 void call(){
   stage('Testing') {
     println "gradle: unit_test()"
-    echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
-    sh 'docker run --rm -u gradle -v ${env.WORKSPACE}:/app -w /app  sisglusdevops/gradle:4.10.3 gradle test'
+    echo "Running ${env.BUILD_ID} on ${env.WORKSPACE}"
+    sh 'docker info'
   }
 }
