2f49542f9cd454a55f32973463df5bb22b127083
xiangxu <iasonliu@gmail.com>
2019-08-07 11:34:40 +0800

    [xiangxu] add build and test

--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,6 +1,4 @@
 void call(){
   println "gradle: unit_test()"
-    steps {
-      sh "docker run --rm -u gradle -v "$PWD":/app -w /app  sisglusdevops/gradle:4.10.3 gradle test"
-      }
+  sh 'docker run --rm -u gradle -v ${env.WORKSPACE}:/app -w /app  sisglusdevops/gradle:4.10.3 gradle test'
 }
