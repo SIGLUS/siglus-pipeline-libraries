466c62c49185e04e73108bb18c36ab76d27cf256
xiangxu <iasonliu@gmail.com>
2019-08-07 12:01:28 +0800

    [xiangxu] add remove steps

--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,9 +1,8 @@
-import hudson.model.*
 void call(){
   stage('Testing') {
-    println "gradle: unit_test()"
-withDockerContainer(args: ' -it  -u gradle -v ${env.WORKSPACE}:/app -w /app ', image: 'siglusdevops/gradle:4.10.3') {
-    gradle test
-}
+        node{
+        println "gradle: unit_test()"
+        sh 'docker run -rm  -u gradle -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle test'
+        }
   }
 }
