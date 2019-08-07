8f7fdcec01839f4480319376f222501eb8abe42c
xiangxu <iasonliu@gmail.com>
2019-08-07 17:33:31 +0800

    [xiangxu] fix build

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -3,7 +3,7 @@ void call(){
     node{
       println "gradle: build()"
       sh 'mkdir -p /tmp/gradle-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u gradle -v /tmp/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle clean build flywayTest'
+      sh 'docker run --rm  -u gradle -v /tmp/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle clean build'
       archiveArtifacts artifacts: 'build/libs/**/*.jar', fingerprint: true
       archiveArtifacts artifacts: 'build/reports/'
     }
