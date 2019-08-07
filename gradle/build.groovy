381f09b8092c697d07375f55eb75fdbc69c9fc99
xiangxu <iasonliu@gmail.com>
2019-08-07 13:41:51 +0800

    [xiangxu] add gradle caches

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -1,14 +1,9 @@
 void call(){
-  stage('Gradle Build') {
+
     node{
       println "gradle: build()"
       sh 'mkdir -p /tmp/gradle-caches/${env.JOB_BASE_NAME}'
       sh 'docker run --rm  -u gradle -v /tmp/gradle-caches/${env.JOB_BASE_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle clean build'
     }
-  }
-  post {
-    success {
-        archiveArtifacts artifacts: 'build/libs/**/*.jar', fingerprint: true
-    }
-  }
+
 }
--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,14 +1,7 @@
 void call(){
-  stage('Testing') {
     node{
       println "gradle: unit_test()"
       sh 'mkdir -p /tmp/gradle-caches/${env.JOB_BASE_NAME}'
       sh 'docker run --rm  -u gradle -v /tmp/gradle-caches/${env.JOB_BASE_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle test'
     }
-  }
-  post {
-    always {
-        junit 'build/reports/**/*.xml'
-    }
-  }
 }
