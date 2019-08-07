02e7317c168ed512d74705c1490cd1a0291f2a0d
xiangxu <iasonliu@gmail.com>
2019-08-07 14:15:55 +0800

    [xiangxu] remove stages

--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,8 +1,7 @@
 void call(){
     node{
       println "gradle: unit_test()"
-      sh 'mkdir -p /tmp/gradle-caches/${env.JOB_BASE_NAME}'
-      sh 'docker run --rm  -u gradle -v /tmp/gradle-caches/${env.JOB_BASE_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app  siglusdevops/gradle:4.10.3 gradle test'
+      sh 'docker run --rm  -u gradle -v /tmp/gradle-caches:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle test'
       junit 'build/reports/**/*.xml'
     }
 }
