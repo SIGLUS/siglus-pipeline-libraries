209fc9f5e6c7f56d8cb8e9f8d0a51be9b34cc819
xiangxu <iasonliu@gmail.com>
2019-08-07 11:46:22 +0800

    [xiangxu] add remove steps

--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,9 +1,7 @@
 void call(){
   stage('Testing') {
-    steps {
-      println "gradle: unit_test()"
-      echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
-      sh 'docker run --rm -u gradle -v ${env.WORKSPACE}:/app -w /app  sisglusdevops/gradle:4.10.3 gradle test'
-    }
+    println "gradle: unit_test()"
+    echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+    sh 'docker run --rm -u gradle -v ${env.WORKSPACE}:/app -w /app  sisglusdevops/gradle:4.10.3 gradle test'
   }
 }
