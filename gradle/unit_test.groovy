bf1ac0c7f81f567cd2542e41f5d63f254f0bcd7b
xiangxu <iasonliu@gmail.com>
2019-08-07 14:30:56 +0800

    [xiangxu] fix gradle caches

--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,6 +1,7 @@
 void call(){
     node{
       println "gradle: unit_test()"
-      sh 'docker run --rm  -u gradle -v /tmp/gradle-caches:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle test'
+      sh 'mkdir -p /tmp/gradle-caches/${JOB_BASE_NAME}'
+      sh 'docker run --rm  -u gradle -v /tmp/gradle-caches/${JOB_BASE_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle test'
     }
 }
