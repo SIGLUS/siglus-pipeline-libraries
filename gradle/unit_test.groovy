2b70dc015311cc2a4dbafecbc5b9e282f75a2241
xiangxu <iasonliu@gmail.com>
2019-08-07 11:43:29 +0800

    [xiangxu] add testing steps

--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,9 +1,9 @@
 void call(){
-
-  steps {
+  stage('Testing') {
+    steps {
       println "gradle: unit_test()"
       echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
       sh 'docker run --rm -u gradle -v ${env.WORKSPACE}:/app -w /app  sisglusdevops/gradle:4.10.3 gradle test'
-   }
-
+    }
+  }
 }
