3cf31282bf8b9d770f11019044f49e6e4d284d8b
xiangxu <iasonliu@gmail.com>
2019-08-07 19:07:11 +0800

    [xiangxu] fix envfile

--- docker/image_build.groovy
+++ docker/image_build.groovy
@@ -9,7 +9,7 @@ def call(){
           IMAGE_NAME=siglusdevops/${PROJECT_NAME#*-}:${IMAGE_VERSION}
           trap $(docker-compose -f docker-compose.builder.yml down --volumes) EXIT
 
-          sudo rm -f .env
+          rm -f .env
           cp $ENV_FILE .env
           if [ "$GIT_BRANCH" != "master" ]; then
               sed -i '' -e "s#^TRANSIFEX_PUSH=.*#TRANSIFEX_PUSH=false#" .env  2>/dev/null || true
