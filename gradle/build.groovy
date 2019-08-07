41a9ac29cc84a4c40150f20be2e67d394a940d3c
xiangxu <iasonliu@gmail.com>
2019-08-07 11:24:01 +0800

    [xiangxu] add build and test

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -1,3 +1,4 @@
 void call(){
     println "gradle: build()"
+    sh 'docker run --rm -u gradle -v "$PWD":/app -w /app  sisglusdevops/gradle:4.10.3 gradle clean build'
 }
--- gradle/unit_test.groovy
+++ gradle/unit_test.groovy
@@ -1,3 +1,4 @@
 void call(){
-      println "gradle: unit_test()"
+  println "gradle: unit_test()"
+  sh 'docker run --rm -u gradle -v "$PWD":/app -w /app  sisglusdevops/gradle:4.10.3 gradle test'
 }
