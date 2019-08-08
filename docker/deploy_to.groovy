647ab6db73c8bbb0b2b1e166e45c0e82a1bcfe7a
xiangxu <iasonliu@gmail.com>
2019-08-08 20:23:14 +0800

    [xiangxu] fix docker-compose file env

--- docker/deploy_to.groovy
+++ docker/deploy_to.groovy
@@ -5,6 +5,8 @@ void call(app_env){
               withEnv(["APP_ENV=${app_env.short_name}", "DOCKER_HOST=tcp://${app_env.hosts}:2376"]) {
                 sh '''
                     rm -f docker-compose*
+                    rm -f .env
+                    rm -f settings.env
                     wget https://raw.githubusercontent.com/SIGLUS/openlmis-ref-distro/master/docker-compose.yml
                     sed -i "s#openlmis#siglusdevops#g" docker-compose.yml
                     GIT_REVISION=$(git rev-parse HEAD)
@@ -12,10 +14,10 @@ void call(app_env){
                     PROJECT_NAME=${JOB_NAME%/*}
                     SERVICE_NAME=${PROJECT_NAME#*-}
                     IMAGE_NAME=`echo "OL_${SERVICE_NAME}_VERSION" |  tr '[:lower:]' '[:upper:]'`
-                    rm -f settings.env
+
                     cp $SETTING_ENV settings.env
                     sed -i "s#<APP_ENV>#${APP_ENV}#g" settings.env
-                    eval ${IMAGE_NAME}=${IMAGE_VERSION}
+                    echo "${IMAGE_NAME}=${IMAGE_VERSION}" > .env
                     docker-compose -H ${DOCKER_HOST} -f docker-compose.yml -p openlmis-ref-distro up --no-deps --force-recreate -d ${SERVICE_NAME}
                 '''
               }
