694d457339320121bb513f4b0a46a932d52025ec
xiangxu <iasonliu@gmail.com>
2019-08-08 17:09:35 +0800

    [xiangxu] fix env

--- docker/deploy_to.groovy
+++ docker/deploy_to.groovy
@@ -6,16 +6,17 @@ void call(app_env){
                 sh '''
                     rm -f docker-compose*
                     wget https://raw.githubusercontent.com/SIGLUS/openlmis-ref-distro/master/docker-compose.yml
+                    sed -i "s#openlmis#siglusdevops#g" docker-compose.yml
                     GIT_REVISION=$(git rev-parse HEAD)
                     IMAGE_VERSION=${BUILD_NUMBER}-${GIT_REVISION}
                     PROJECT_NAME=${JOB_NAME%/*}
-                    IMAGE_NAME=siglusdevops/${PROJECT_NAME#*-}:${IMAGE_VERSION}
+                    SERVICE_NAME=${PROJECT_NAME#*-}
                     rm -f settings.env
                     cp $SETTING_ENV settings.env
                     sed -i "s#<APP_ENV>#${APP_ENV}#g" settings.env
-                    export OL_${PROJECT_NAME#*-}_VERSION=${IMAGE_NAME}
+                    export OL_${SERVICE_NAME^^}_VERSION=${IMAGE_VERSION}
                     printenv
-                    # docker-compose -f docker-compose.yml -p openlmis-ref-distro up -d --force-recreate ${PROJECT_NAME#*-}
+                    docker-compose -f docker-compose.yml -p openlmis-ref-distro up -d --force-recreate ${SERVICE_NAME}
                 '''
               }
             }
