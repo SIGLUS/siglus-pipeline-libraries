8f2232775b493515dee6c04b46b5cd0cb97e8a15
xiangxu <iasonliu@gmail.com>
2019-08-08 16:17:49 +0800

    [xiangxu] add docker-compose deploy

--- docker/deploy_to.groovy
+++ docker/deploy_to.groovy
@@ -1,20 +1,22 @@
 void call(app_env){
   stage "Deploy to ${app_env.long_name}", {
-    withCredentials([file(credentialsId: 'setting_env', variable: 'SETTING_ENV')]) {
-      sh '''
-        wget https://raw.githubusercontent.com/SIGLUS/openlmis-ref-distro/master/docker-compose.yml
-        GIT_REVISION=$(git rev-parse HEAD)
-        IMAGE_VERSION=${BUILD_NUMBER}-${GIT_REVISION}
-        PROJECT_NAME=${JOB_NAME%/*}
-        IMAGE_NAME=siglusdevops/${PROJECT_NAME#*-}:${IMAGE_VERSION}
-        rm -f settings.env
-        cp $SETTING_ENV settings.env
-        sed -i "s#<APP_ENV>#${app_env.short_name}#g" settings.env
-        DEPLOY_SERVICE=OL_${PROJECT_NAME#*-}_VERSION
-        eval "${DEPLOY_SERVICE^^}"='${IMAGE_NAME}'
-        printenv
-        docker-compose -f docker-compose.yml -p openlmis-ref-distro up -d --force-recreate ${PROJECT_NAME#*-}
-      '''
-    }
-  }
+      node{
+            withCredentials([file(credentialsId: 'setting_env', variable: 'SETTING_ENV')]) {
+            sh '''
+                wget https://raw.githubusercontent.com/SIGLUS/openlmis-ref-distro/master/docker-compose.yml
+                GIT_REVISION=$(git rev-parse HEAD)
+                IMAGE_VERSION=${BUILD_NUMBER}-${GIT_REVISION}
+                PROJECT_NAME=${JOB_NAME%/*}
+                IMAGE_NAME=siglusdevops/${PROJECT_NAME#*-}:${IMAGE_VERSION}
+                rm -f settings.env
+                cp $SETTING_ENV settings.env
+                sed -i "s#<APP_ENV>#${app_env.short_name}#g" settings.env
+                DEPLOY_SERVICE=OL_${PROJECT_NAME#*-}_VERSION
+                eval "${DEPLOY_SERVICE^^}"='${IMAGE_NAME}'
+                printenv
+                docker-compose -f docker-compose.yml -p openlmis-ref-distro up -d --force-recreate ${PROJECT_NAME#*-}
+            '''
+            }
+        }
+      }
 }
