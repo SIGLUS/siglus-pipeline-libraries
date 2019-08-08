56943bf436a9dd9fbce0c18a993ef05bce8cdb8a
xiangxu <iasonliu@gmail.com>
2019-08-08 19:02:32 +0800

    [xiangxu] update dir

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -2,8 +2,8 @@ void call(){
   stage('Building'){
     node{
       println "gradle: build()"
-      sh 'mkdir -p /efs/gradle-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u gradle -v /efs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle clean build'
+      sh 'mkdir -p /ebs/gradle-caches/${JOB_NAME}'
+      sh 'docker run --rm  -u gradle -v /ebs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle clean build'
     }
   }
 }
--- gradle/pmd_test.groovy
+++ gradle/pmd_test.groovy
@@ -2,8 +2,8 @@ void call(){
   stage('PMD Testing'){
     node{
       println "gradle: pmd_test()"
-      sh 'mkdir -p /efs/gradle-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u gradle -v /efs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle pmdMain checkstyleMain'
+      sh 'mkdir -p /ebs/gradle-caches/${JOB_NAME}'
+      sh 'docker run --rm  -u gradle -v /ebs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle pmdMain checkstyleMain'
     }
   }
 }
