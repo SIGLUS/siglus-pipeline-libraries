3d6ff7d39e477b930c8892edbaaa06a1a813ff6b
xiangxu <iasonliu@gmail.com>
2019-08-08 16:00:29 +0800

    [xiangxu] add deploy_to

--- pipeline_common/pipeline_config.groovy
+++ pipeline_common/pipeline_config.groovy
@@ -4,6 +4,21 @@
     these should be configurations common across
     all apps governed by this config.
 */
+application_environments{
+  dev{
+    short_name = "dev"
+    long_name = "Development"
+  }
+  test{
+    short_name = "test"
+    long_name = "Test"
+  }
+  prod{
+    short_name = "prod"
+    long_name = "Production"
+  }
+}
+
 libraries{
   merge = true
   docker
