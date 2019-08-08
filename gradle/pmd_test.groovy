92e23ca99bdff9a786ea04c381d037b9eb7b1bc3
xiangxu <iasonliu@gmail.com>
2019-08-08 13:50:19 +0800

    [xiangxu] fix gradle caches

--- gradle/pmd_test.groovy
+++ gradle/pmd_test.groovy
@@ -3,7 +3,7 @@ void call(){
     node{
       println "gradle: pmd_test()"
       sh 'mkdir -p /efs/gradle-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u gradle -v /efs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle pmdMain pmdTest checkstyleMain checkstyleTest'
+      sh 'docker run --rm  -u gradle -v /efs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle pmdMain checkstyleMain'
     }
   }
 }
