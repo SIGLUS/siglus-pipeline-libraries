e33772da7c60b711cb5d8dd30e7920abb0428794
xiangxu <iasonliu@gmail.com>
2019-08-08 13:01:28 +0800

    [xiangxu] update build images

--- docker/build_image.groovy
+++ docker/build_image.groovy
@@ -5,7 +5,7 @@ def call(){
       GIT_REVISION=$(git rev-parse HEAD)
       IMAGE_VERSION=${BUILD_NUMBER}-${GIT_REVISION}
       PROJECT_NAME=${JOB_NAME%/*}
-      IMAGE_NAME=siglusdevops/${PROJECT_NAME}:${IMAGE_VERSION}
+      IMAGE_NAME=siglusdevops/${PROJECT_NAME#*-}:${IMAGE_VERSION}
       docker build -t ${IMAGE_NAME} .
       docker push ${IMAGE_NAME}
       docker rmi ${IMAGE_NAME}
