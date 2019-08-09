a6054c826d59546b45ea7dac216e024db6cf6c20
xiangxu <iasonliu@gmail.com>
2019-08-09 17:52:54 +0800

    [xiangxu] add testing

--- grunt/pmd_test.groovy
+++ grunt/pmd_test.groovy
@@ -3,7 +3,7 @@ void call(){
     node{
       println "grunt: pmd_test()"
       sh 'mkdir -p /ebs/node-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u nodejs -v ${PWD}:/app -v /ebs/node-caches/${JOB_NAME}:/app/node_modules -w /app siglusdevops/dev-ui:latest build.sh'
+      sh 'docker run --rm  -u nodejs -v ${PWD}:/app -v /ebs/node-caches/${JOB_NAME}:/app/node_modules -w /app siglusdevops/dev-ui:latest /dev-ui/build.sh'
     }
   }
 }
