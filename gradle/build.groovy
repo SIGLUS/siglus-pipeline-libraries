5b83f48310b8c1352dff13662fe7ac6753543025
xiangxu <iasonliu@gmail.com>
2019-08-09 12:34:02 +0800

    [xiangxu] fix test with log

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -3,7 +3,7 @@ void call(){
     node{
       println "gradle: build()"
       sh 'mkdir -p /ebs/gradle-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u gradle -v /ebs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -v /ebs/node-caches:/app/node_modules -w /app siglusdevops/gradle:4.10.3 gradle clean build'
+      sh 'docker run --rm  -u gradle -add-host=log:127.0.0.1 -v /ebs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -v /ebs/node-caches:/app/node_modules -w /app siglusdevops/gradle:4.10.3 gradle clean build'
     }
   }
 }
