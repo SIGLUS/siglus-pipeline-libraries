b3a74dd896d9f0ae3e1e7e36b2a2f2220e821e5d
xiangxu <iasonliu@gmail.com>
2019-08-09 16:39:58 +0800

    [xiangxu] add grunt libraries

---
+++ grunt/pmd_test.groovy
@@ -0,0 +1,9 @@
+void call(){
+  stage('Testing'){
+    node{
+      println "grunt: pmd_test()"
+      sh 'mkdir -p /ebs/node-caches/${JOB_NAME}'
+      sh 'docker run --rm  -u nodejs -v ${PWD}:/app -v /ebs/node-caches/${JOB_NAME}:/app/node_modules -w /app siglusdevops/dev-ui:latest grunt karma:unit'
+    }
+  }
+}
