5a4cdd5f437bda6d977332f3d1a870fbe3534339
xiangxu <iasonliu@gmail.com>
2019-08-09 11:30:33 +0800

    [xiangxu] add node caches

--- gradle/build.groovy
+++ gradle/build.groovy
@@ -3,7 +3,7 @@ void call(){
     node{
       println "gradle: build()"
       sh 'mkdir -p /ebs/gradle-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u gradle -v /ebs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle clean build'
+      sh 'docker run --rm  -u gradle -v /ebs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -v /ebs/node-caches:/app/node_modules -w /app siglusdevops/gradle:4.10.3 gradle clean build'
     }
   }
 }
