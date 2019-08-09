4dcbf9e8053fada44a0404729b313d0b219ff21d
xiangxu <iasonliu@gmail.com>
2019-08-09 11:18:52 +0800

    [xiangxu] fix gradle running

--- gradle/pmd_test.groovy
+++ gradle/pmd_test.groovy
@@ -3,7 +3,7 @@ void call(){
     node{
       println "gradle: pmd_test()"
       sh 'mkdir -p /ebs/gradle-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u gradle -v /ebs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle gradle pmdMain pmdTest checkstyleMain checkstyleTest'
+      sh 'docker run --rm  -u gradle -v /ebs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle pmdMain pmdTest checkstyleMain checkstyleTest'
     }
   }
 }
