6ae3aa555b97eea38cd9a26ecaa6286e8f92eef1
Baiyuan Wang <bywang@thoughtworks.com>
2019-08-12 16:52:36 +0800

    [Baiyuan] Add npm global cache

--- grunt/pmd_test.groovy
+++ grunt/pmd_test.groovy
@@ -3,7 +3,7 @@ void call(){
     node{
       println "grunt: pmd_test()"
       sh 'mkdir -p /ebs/node-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u nodejs -v ${PWD}:/app -v /ebs/node-caches/${JOB_NAME}:/app/node_modules -w /app siglusdevops/dev-ui:latest /dev-ui/build.sh'
+      sh 'docker run --rm  -u nodejs -v ${PWD}:/app -v /ebs/node-caches/global:/home/nodejs/.npm -v /ebs/node-caches/${JOB_NAME}:/app/node_modules -w /app siglusdevops/dev-ui:latest /dev-ui/build.sh'
     }
   }
 }
