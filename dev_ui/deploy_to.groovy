b046a55f09adbec2c668ea44d25d5fbc57961be6
Baiyuan Wang <bywang@thoughtworks.com>
2019-08-13 15:49:42 +0800

    [Baiyuan] Import pipeline-build-step lib

--- dev_ui/deploy_to.groovy
+++ dev_ui/deploy_to.groovy
@@ -1,3 +1,4 @@
+@Library('org.jenkins-ci.plugins:pipeline-build-step:2.9')
 import org.jenkinsci.plugins.workflow.support.steps.build
 void call(app_env){
   stage("Integrated with ReferenceUI: ${app_env.long_name}") {
