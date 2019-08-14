b89b5c3af00ea11a2966621d28d4f2a7caec0a65
Baiyuan Wang <bywang@thoughtworks.com>
2019-08-14 15:05:22 +0800

    [Baiyuan] Rename build to build_app

--- dev_ui/deploy_to.groovy
+++ dev_ui/deploy_to.groovy
@@ -1,7 +1,7 @@
-@Library('pipeline-build-step')
-import org.jenkinsci.plugins.workflow.support.steps.build
 void call(app_env){
   stage("Integrated with ReferenceUI: ${app_env.long_name}") {
-    build job: 'openlmis-reference-ui/master', wait: false
+    node{ 
+      build job: 'openlmis-reference-ui/master', wait: false
+    }
   }
 }
