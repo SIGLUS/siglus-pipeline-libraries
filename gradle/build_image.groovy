b1f51d88c3ac19e90c3567402785bf51f288f298
Baiyuan Wang <bywang@thoughtworks.com>
2019-08-14 11:06:18 +0800

    [Baiyuan] Push docker image with lastest tag

--- gradle/build_image.groovy
+++ gradle/build_image.groovy
@@ -5,10 +5,13 @@ def call(){
       GIT_REVISION=$(git rev-parse HEAD)
       IMAGE_VERSION=${BUILD_NUMBER}-${GIT_REVISION}
       PROJECT_NAME=${JOB_NAME%/*}
-      IMAGE_NAME=siglusdevops/${PROJECT_NAME#*-}:${IMAGE_VERSION}
+      IMAGE_REPO=siglusdevops/${PROJECT_NAME#*-}
+      IMAGE_NAME=${IMAGE_REPO}:${IMAGE_VERSION}
       docker build -t ${IMAGE_NAME} .
+      docker tag ${IMAGE_NAME} ${IMAGE_REPO}:latest
       docker push ${IMAGE_NAME}
-      docker rmi ${IMAGE_NAME}
+      docker push ${IMAGE_REPO}:latest
+      docker rmi ${IMAGE_NAME} ${IMAGE_REPO}:latest
       echo "*************Image name is [${IMAGE_NAME}]*************"
       '''
     }
