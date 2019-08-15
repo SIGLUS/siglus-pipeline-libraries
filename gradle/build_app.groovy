9fcf2edb2f91f1ea67d5c2f23c93aee1d6c8140c
xiangxu <iasonliu@gmail.com>
2019-08-15 23:20:49 +0800

    [xiangxu] remove all caches for debug

--- gradle/build_app.groovy
+++ gradle/build_app.groovy
@@ -4,7 +4,7 @@ void call(){
       println "gradle: build()"
       sh 'mkdir -p /ebs2/gradle-caches/${JOB_NAME}'
       sh 'mkdir -p /ebs2/node-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u gradle --add-host=log:127.0.0.1 -v /ebs/gradle-caches/${JOB_NAME}:/home/gradle/.gradle/caches -v ${PWD}:/app -v /ebs2/gradle-caches/${JOB_NAME}:/app/node_modules -w /app siglusdevops/gradle:4.10.3 gradle clean build'
+      sh 'docker run --rm  -u gradle --add-host=log:127.0.0.1 -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle clean build'
       checkstyle pattern: '**/build/reports/checkstyle/*.xml'
       pmd pattern: '**/build/reports/pmd/*.xml'
       junit '**/build/test-results/*/*.xml'
