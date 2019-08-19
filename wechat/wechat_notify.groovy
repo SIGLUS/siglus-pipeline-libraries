1d8a2f0d2d5a76bd33edf9b41ca8d58cb786d32a
xiangxu <iasonliu@gmail.com>
2019-08-19 17:21:19 +0800

    [xiangxu] test wechat

--- wechat/wechat_notify.groovy
+++ wechat/wechat_notify.groovy
@@ -1,38 +1,40 @@
 @AfterStep
 def call(context){
   node{
-    switch(context.status){
-        case null: // no result set yet means success
-        case "SUCCESS":
-          this.post_message("info", "BUILD SUCCESS")
-          break;
-        case "FAILURE":
-          this.post_message("warning", "Build Failure")
-          break;
-        default:
-          echo "WeChat Notifier doing nothing: ${context.status}"
+    withCredentials([string(credentialsId: 'wechat_token', variable: 'WECHAT_TOKEN')]){
+      switch(context.status){
+          case null: // no result set yet means success
+          case "SUCCESS":
+            sh '''
+              USER_NAME=$(git show -s --pretty=%an)
+              curl -s 'http://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${WECHAT_TOKEN}' \
+                  -H 'Content-Type: application/json' \
+                  -d '
+                  {
+                    "msgtype": "markdown",
+                    "markdown": {
+                        "content": "<font color=\"info\">Build Success: by ${USER_NAME}</font>\n>JOB_NAME: ${env.JOB_NAME}"
+                    }
+                  }'
+            '''
+            break;
+          case "FAILURE":
+            sh '''
+              USER_NAME=$(git show -s --pretty=%an)
+              curl -s 'http://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${WECHAT_TOKEN}' \
+                  -H 'Content-Type: application/json' \
+                  -d '
+                  {
+                    "msgtype": "markdown",
+                    "markdown": {
+                        "content": "<font color=\"warning\">Build Failure: by ${USER_NAME}</font>\n>JOB_NAME: ${env.JOB_NAME}"
+                    }
+                  }'
+            '''
+            break;
+          default:
+            echo "WeChat Notifier doing nothing: ${context.status}"
+      }
     }
-  }
-}
-
-void post_message(color, message){
-  withCredentials([string(credentialsId: 'wechat_token', variable: 'WECHAT_TOKEN')]){
-    sh '''
-    USER_NAME=$(git show -s --pretty=%an)
-    cat <<EOT > send.sh
-    curl -s 'http://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${env.WECHAT_TOKEN}' \
-          -H 'Content-Type: application/json' \
-          -d '
-          {
-            "msgtype": "markdown",
-            "markdown": {
-                "content": "<font color=\"${color}\">${message}: by ${USER_NAME}</font>\n
-                >JOB_NAME: ${env.JOB_NAME} \n
-                >[JOB_URL](${env.JOB_URL}) \n"
-            }
-          }'
-    EOT
-    chmod a+x send.sh && send.sh
-    '''
   }
 }
