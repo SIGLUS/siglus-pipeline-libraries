3e3de2e61bdbc872c4f7cd5fc44451189e767a6e
xiangxu <iasonliu@gmail.com>
2019-08-19 17:07:47 +0800

    [xiangxu] testing send wechat

--- wechat/wechat_notify.groovy
+++ wechat/wechat_notify.groovy
@@ -19,17 +19,19 @@ void post_message(color, message){
   withCredentials([string(credentialsId: 'wechat_token', variable: 'WECHAT_TOKEN')]){
     sh '''
     USER_NAME=$(git show -s --pretty=%an)
-    echo "curl -s 'http://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${env.WECHAT_TOKEN}' \
-      -H 'Content-Type: application/json' \
-      -d '
-      {
-        "msgtype": "markdown",
-        "markdown": {
-            "content": "<font color=\"${color}\">${message}: by ${USER_NAME}</font>\n
-            >JOB_NAME: ${env.JOB_NAME} \n
-            >[JOB_URL](${env.JOB_URL}) \n"
-        }
-      }'" > send.sh
+    cat <<EOT >> send.sh
+    curl -s 'http://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${env.WECHAT_TOKEN}' \
+          -H 'Content-Type: application/json' \
+          -d '
+          {
+            "msgtype": "markdown",
+            "markdown": {
+                "content": "<font color=\"${color}\">${message}: by ${USER_NAME}</font>\n
+                >JOB_NAME: ${env.JOB_NAME} \n
+                >[JOB_URL](${env.JOB_URL}) \n"
+            }
+          }'
+    EOT
     chmod a+x send.sh && send.sh
     '''
   }
