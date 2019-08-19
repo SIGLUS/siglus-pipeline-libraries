7d4c07b117f8b99282317751cbbe0c797aacb2fd
Baiyuan Wang <bywang@thoughtworks.com>
2019-08-19 11:44:38 +0800

    [Baiyuan] Test build job in afterStep hook

--- dev_ui/deploy_to.groovy
+++ dev_ui/deploy_to.groovy
@@ -5,3 +5,10 @@ void call(app_env){
     }
   }
 }
+
+@AfterStep
+void call(context){
+    if (context.step.equals("build_app")){
+        build job: 'openlmis-reference-ui/master', wait: false
+    }
+}
