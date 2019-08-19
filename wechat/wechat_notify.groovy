1de1e427fa987fa3d32677a72c275fd1c1c6e156
xiangxu <iasonliu@gmail.com>
2019-08-19 19:46:44 +0800

    [xiangxu] debug stagename

--- wechat/wechat_notify.groovy
+++ wechat/wechat_notify.groovy
@@ -3,13 +3,14 @@ def call(context){
   node{
     withCredentials([string(credentialsId: 'wechat_token', variable: 'WECHAT_TOKEN')]){
       sh 'printenv'
+      sh '${STAGE_NAME}'
       switch(context.status){
           case null: // no result set yet means success
           case "SUCCESS":
             sh '''
               GIT_USER_NAME=$(git show -s --pretty=%an)
               cat << EOF > data.json
-{"msgtype": "markdown","markdown": {"content": "<font color=\\"info\\">Build Success: commit by **${GIT_USER_NAME}**</font>\\n>**${JOB_NAME}:** [Details](${JOB_URL})"}}
+{"msgtype": "markdown","markdown": {"content": "<font color=\\"info\\">Build Success: commit by ${GIT_USER_NAME}</font>\\n>**${JOB_NAME}:** [Details](${RUN_DISPLAY_URL})"}}
 EOF
             '''
             sh '''
@@ -22,7 +23,7 @@ EOF
             sh '''
               GIT_USER_NAME=$(git show -s --pretty=%an)
               cat << EOF > data.json
-{"msgtype": "markdown","markdown": {"content": "<font color=\\"warning\\">Build Failure: commit by **${GIT_USER_NAME}**</font>\\n>**${JOB_NAME}:** [Details](${JOB_URL})","mentioned_list":[\"@all\"]}}
+{"msgtype": "markdown","markdown": {"content": "<font color=\\"warning\\">Build Failure: commit by ${GIT_USER_NAME}</font>\\n>**${JOB_NAME}:** [Details](${RUN_DISPLAY_URL})","mentioned_list":[\"@all\"]}}
 EOF
             '''
             sh '''
