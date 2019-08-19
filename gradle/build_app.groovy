ea7760b36e9f1d69956e816aab9b6753ac47f3b3
xiangxu <iasonliu@gmail.com>
2019-08-19 13:58:56 +0800

    [xiangxu] fix config

--- gradle/build_app.groovy
+++ gradle/build_app.groovy
@@ -1,10 +1,12 @@
 void call(){
   stage('Building'){
     node{
-      println "gradle: build()"
-      sh 'mkdir -p /ebs2/gradle-caches/${JOB_NAME}'
-      sh 'mkdir -p /ebs2/node-caches/${JOB_NAME}'
-      sh 'docker run --rm  -u gradle --add-host=log:127.0.0.1 -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle clean build -x checkstyleMain -x checkstyleTest -x checkstyleIntegrationTest -x pmdMain -x pmdTest -x pmdIntegrationTest'
+      ansiColor('xterm') {
+        println "gradle: build()"
+        sh 'mkdir -p /ebs2/gradle-caches/${JOB_NAME}'
+        sh 'mkdir -p /ebs2/node-caches/${JOB_NAME}'
+        sh 'docker run --rm  -u gradle --add-host=log:127.0.0.1 -v ${PWD}:/app -w /app siglusdevops/gradle:4.10.3 gradle clean build -x checkstyleMain -x checkstyleTest -x checkstyleIntegrationTest -x pmdMain -x pmdTest -x pmdIntegrationTest'
+      }
       checkstyle pattern: '**/build/reports/checkstyle/*.xml'
       pmd pattern: '**/build/reports/pmd/*.xml'
       junit '**/build/test-results/*/*.xml'
