df6eaa2688837b2920f390388f3b89e3a7a0a306
xiangxu <iasonliu@gmail.com>
2019-08-19 12:01:27 +0800

    [xiangxu] add notify

--- dev_ui/deploy_to.groovy
+++ dev_ui/deploy_to.groovy
@@ -1,14 +1,8 @@
 void call(app_env){
   stage("Integrated with ReferenceUI: ${app_env.long_name}") {
-    node{ 
+    node{
       build job: 'openlmis-reference-ui/master', wait: false
     }
   }
 }
 
-@AfterStep
-void notify_build_reference_ui(context){
-    if (context.step.equals("build_app")){
-        build job: 'openlmis-reference-ui/master', wait: false
-    }
-}
