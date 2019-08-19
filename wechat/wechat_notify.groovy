9b2b27d21c599d985b5139489010d37a150e85ba
xiangxu <iasonliu@gmail.com>
2019-08-19 18:42:24 +0800

    [xiangxu] fix shell

--- wechat/wechat_notify.groovy
+++ wechat/wechat_notify.groovy
@@ -8,30 +8,23 @@ def call(context){
             sh '''
               GIT_USER_NAME=$(git show -s --pretty=%an)
               cat << EOF > data.json
-                {
-                  "msgtype": "markdown",
-                  "markdown": {
-                      "content": "<font color=\"info\">Build Success: by ${GIT_USER_NAME}</font>\n>JOB_NAME: ${JOB_NAME}"
-                  }
-                }
-              EOF
-              curl -s "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${WECHAT_TOKEN}" \
+{"msgtype": "markdown","markdown": {"content": "<font color=\"info\">Build Success: by ${GIT_USER_NAME}</font>\n>JOB_NAME: ${JOB_NAME}"}}
+EOF
+            '''
+            sh '''
+                curl -s "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${WECHAT_TOKEN}" \
                   -H 'Content-Type: application/json' \
                   -d @./data.json
-            '''
+              '''
             break;
           case "FAILURE":
             sh '''
               GIT_USER_NAME=$(git show -s --pretty=%an)
               cat << EOF > data.json
-                {
-                  "msgtype": "markdown",
-                  "markdown": {
-                      "content": "<font color=\"warning\">Build Success: by ${GIT_USER_NAME}</font>\n>JOB_NAME: ${JOB_NAME}",
-                      "mentioned_list":["@all"]
-                  }
-                }
-              EOF
+{"msgtype": "markdown","markdown": {"content": "<font color=\"warning\">Build Success: by ${GIT_USER_NAME}</font>\n>JOB_NAME: ${JOB_NAME}","mentioned_list":[\"\@all\"]}}
+EOF
+            '''
+            sh '''
               curl -s "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${WECHAT_TOKEN}" \
                   -H 'Content-Type: application/json' \
                   -d @./data.json
