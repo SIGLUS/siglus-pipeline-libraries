ebe4b88330d2fde6ad679df41513412206163a13
xiangxu <iasonliu@gmail.com>
2019-08-19 13:49:56 +0800

    [xiangxu] testing dir

--- slack/slack_notify.groovy
+++ slack/slack_notify.groovy
@@ -1,6 +1,4 @@
 @AfterStep
 void call(context){
-    if (context.step.equals("build") && context.status.equals("FAILURE")){
-        slackSend color: '#ff0000', message: "Build Failure: ${env.JOB_URL}"
-    }
+    slackSend color: '#ff0000', message: "Build Failure: ${env.JOB_URL}"
 }
