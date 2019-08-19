1e4d2eb24b664cd82caf8e051b6aac20f4b5dc75
xiangxu <iasonliu@gmail.com>
2019-08-19 13:43:25 +0800

    [xiangxu] testing dir

--- dev_ui/deploy_to.groovy
+++ dev_ui/deploy_to.groovy
@@ -1,7 +1,7 @@
 void call(app_env){
   node{
     stage("Integrated with ReferenceUI: ${app_env.long_name}") {
-        build job: 'openlmis-reference-ui/master', wait: false
+        build job: '../openlmis-reference-ui/master', wait: false
     }
   }
 }
