c8c8df7947cb678c8e66e20ffffad789eda0e3ff
xiangxu <iasonliu@gmail.com>
2019-08-19 16:40:54 +0800

    [xiangxu] fix with env

--- wechat/wechat_notify.groovy
+++ wechat/wechat_notify.groovy
@@ -1,17 +1,18 @@
 @AfterStep
 def call(context){
-    withCredentials([string(credentialsId: 'wechat_token', variable: 'WECHAT_TOKEN')])
-    switch(context.status){
-        case null: // no result set yet means success
-        case "SUCCESS":
-          this.post_message("info", "BUILD SUCCESS", ${WECHAT_TOKEN})
-          break;
-        case "FAILURE":
-          this.post_message("warning", "Build Failure", ${WECHAT_TOKEN})
-          break;
-        default:
-          echo "WeChat Notifier doing nothing: ${context.status}"
-    }
+    withCredentials([string(credentialsId: 'wechat_token', variable: 'WECHAT_TOKEN')]){
+      switch(context.status){
+          case null: // no result set yet means success
+          case "SUCCESS":
+            this.post_message("info", "BUILD SUCCESS", ${WECHAT_TOKEN})
+            break;
+          case "FAILURE":
+            this.post_message("warning", "Build Failure", ${WECHAT_TOKEN})
+            break;
+          default:
+            echo "WeChat Notifier doing nothing: ${context.status}"
+      }
+  }
 }
 
 void post_message(color, message, key_token){
