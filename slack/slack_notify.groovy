ff3f05f0fd55ab4561ee56a99bb91cf1d5e685e4
xiangxu <iasonliu@gmail.com>
2019-08-20 19:15:04 +0800

    [xiangxu] fix build slack message

--- slack/slack_notify.groovy
+++ slack/slack_notify.groovy
@@ -3,21 +3,24 @@ def call(context){
     switch(context.status){
         case null: // no result set yet means success
         case "SUCCESS":
-          notifyStage('good', "Build Successful.")
+            def icons = [":unicorn_face:", ":beer:", ":bee:", ":man_dancing:",
+                ":party_parrot:", ":ghost:", ":dancer:", ":scream_cat:"]
+            def randomIndex = (new Random()).nextInt(icons.size())
+            def message = "@here Build <${env.BUILD_URL}|${currentBuild.displayName}> " +
+                "Build Success. ${icons[randomIndex]}"
+            slackSend message: ${message}, color: 'good'
           break;
         case "FAILURE":
-          notifyStage('#ff0000', "Build Failure.")
+            def icons = [":unicorn_face:", ":beer:", ":bee:", ":man_dancing:",
+                ":party_parrot:", ":ghost:", ":dancer:", ":scream_cat:"]
+            def randomIndex = (new Random()).nextInt(icons.size())
+            def message = "@here Build <${env.BUILD_URL}|${currentBuild.displayName}> " +
+                "Build Failure. ${icons[randomIndex]}"
+            slackSend message: ${message}, color: '#ff0000'
           break;
         default:
           echo "Slack Notifier doing nothing: ${context.status}"
     }
 }
 
-def notifyStage(color, status_message) {
-  def icons = [":unicorn_face:", ":beer:", ":bee:", ":man_dancing:",
-    ":party_parrot:", ":ghost:", ":dancer:", ":scream_cat:"]
-  def randomIndex = (new Random()).nextInt(icons.size())
-  def message = "@here Build <${env.BUILD_URL}|${currentBuild.displayName}> " +
-      "${status_message} ${icons[randomIndex]}"
-  slackSend message: ${message}, color: ${color}
-}
+dd
