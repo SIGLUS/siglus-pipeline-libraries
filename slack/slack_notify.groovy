a66f9332bdb6e602646cf9e9b4b52944804eaf9d
xiangxu <iasonliu@gmail.com>
2019-08-20 18:56:08 +0800

    [xiangxu] setting salck notify

--- slack/slack_notify.groovy
+++ slack/slack_notify.groovy
@@ -3,12 +3,21 @@ def call(context){
     switch(context.status){
         case null: // no result set yet means success
         case "SUCCESS":
-          slackSend color: "good", message: "Build Successful: ${env.JOB_URL}"
+          notifyStage('good', "Build Successful.")
           break;
         case "FAILURE":
-          slackSend color: '#ff0000', message: "Build Failure: ${env.JOB_URL}"
+          notifyStage('#ff0000', "Build Failure.")
           break;
         default:
           echo "Slack Notifier doing nothing: ${context.status}"
     }
+}
+
+def notifyStage(color, status_message) {
+  def icons = [":unicorn_face:", ":beer:", ":bee:", ":man_dancing:",
+    ":party_parrot:", ":ghost:", ":dancer:", ":scream_cat:"]
+  def randomIndex = (new Random()).nextInt(icons.size())
+  def message = "@here Build <${env.BUILD_URL}|${currentBuild.displayName}> " +
+      "${status_message} ${icons[randomIndex]}"
+  slackSend(message: message, color: ${color})
 }
