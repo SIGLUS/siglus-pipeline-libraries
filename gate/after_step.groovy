ab1db6688092f2b98bf4e4522e5cd5ca169b0355
xiangxu <iasonliu@gmail.com>
2019-08-21 13:47:00 +0800

    [xiangxu] fix timeout for qa deploy

---
+++ gate/after_step.groovy
@@ -0,0 +1,8 @@
+@AfterStep
+def call(context){
+    script {
+        if (currentBuild.result == null) {
+            currentBuild.result = 'SUCCESS'
+        }
+    }
+}
--- gate/deploy_gate.groovy
+++ gate/deploy_gate.groovy
@@ -1,4 +1,16 @@
-@BeforeStep
 void call(context){
-    expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
-}
+   stage('wait'){
+    node{
+           try {
+                timeout(5) {
+                    input(message: 'Deploy this build to QA?')
+                }
+              }
+            } catch (err) {
+              def user = err.getCauses()[0].getUser()
+              if('SYSTEM' == user.toString()) { //timeout
+                currentBuild.result = "SUCCESS"
+              }
+    }
+   }
+  }
