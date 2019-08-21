d18a9604ef9b92d3cb69e543c73a19cf67d40f8e
xiangxu <iasonliu@gmail.com>
2019-08-21 12:02:57 +0800

    [xiangxu] fix qa deploy gate

--- gate/deploy_gate.groovy
+++ gate/deploy_gate.groovy
@@ -1,11 +1,4 @@
 @BeforeStep
 void call(context){
-    node {
-        expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
-        if (context.step.contains("QA")){
-            timeout(time:5, unit:'DAYS') {
-                input message:'Approve deployment to QA?' , ok: 'Yes'
-            }
-        }
-    }
+    expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
 }
