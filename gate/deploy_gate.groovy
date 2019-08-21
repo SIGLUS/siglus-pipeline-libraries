f6c031a59b659485508733bff5a212b73bc94ecd
xiangxu <iasonliu@gmail.com>
2019-08-21 10:53:59 +0800

    [xiangxu] add more info for salck notify

--- gate/deploy_gate.groovy
+++ gate/deploy_gate.groovy
@@ -1,13 +1,15 @@
 @BeforeStep
 void call(context){
-    if (context.step.equals("Deploy to Qa")){
-        when {
-            allOf {
-            expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
-            }
+    when {
+        allOf {
+        expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
         }
+    }
+    sh 'echo ${context}'
+    if (context.step.equals("Deploy to Qa")){
+
         timeout(time:5, unit:'DAYS') {
-            input message:'Approve deployment to QA?'
+            input message:'Approve deployment to QA?' , ok: 'Yes'
         }
     }
 }
--- slack/slack_notify.groovy
+++ slack/slack_notify.groovy
@@ -1,21 +1,21 @@
 @AfterStep
 def call(context){
+    def commitUser = sh(returnStdout: true, script: "git show -s --pretty=%an").trim()
+    def commitChangeset = sh(returnStdout: true, script: 'git diff-tree --no-commit-id --name-status -r HEAD').trim()
+    def icons = [":unicorn_face:", ":beer:", ":bee:", ":man_dancing:",
+        ":party_parrot:", ":ghost:", ":dancer:", ":scream_cat:"]
+    def randomIndex = (new Random()).nextInt(icons.size())
+    echo "${context.step}"
     switch(context.status){
         case null: // no result set yet means success
         case "SUCCESS":
-            def icons = [":unicorn_face:", ":beer:", ":bee:", ":man_dancing:",
-                ":party_parrot:", ":ghost:", ":dancer:", ":scream_cat:"]
-            def randomIndex = (new Random()).nextInt(icons.size())
-            def message = "@here Build ${env.JOB_NAME} ${context.step} <${env.BUILD_URL}|${currentBuild.displayName}> " +
-                "Build Success. ${icons[randomIndex]}"
+            def message = "@here Build ${env.JOB_NAME} ${env.STAGE_NAME} <${env.BUILD_URL}|${currentBuild.displayName}> " +
+                "Build Success. ${icons[randomIndex]}\n" + "```${commitChangeset}```"
             slackSend message: "${message}", color: 'good'
           break;
         case "FAILURE":
-            def icons = [":unicorn_face:", ":beer:", ":bee:", ":man_dancing:",
-                ":party_parrot:", ":ghost:", ":dancer:", ":scream_cat:"]
-            def randomIndex = (new Random()).nextInt(icons.size())
-            def message = "@here Build ${env.JOB_NAME} ${context.step} <${env.BUILD_URL}|${currentBuild.displayName}> " +
-                "Build Failure. ${icons[randomIndex]}"
+            def message = "@here Build ${env.JOB_NAME} ${env.STAGE_NAME} <${env.BUILD_URL}|${currentBuild.displayName}> " +
+                "Build Failure. ${icons[randomIndex]}\n" + "```${commitChangeset}```"
             slackSend message: "${message}", color: '#ff0000'
           break;
         default:
