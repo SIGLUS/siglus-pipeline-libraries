b9a505eede9a02e61016b6278bb4d314301868e3
xiangxu <iasonliu@gmail.com>
2019-08-21 11:56:36 +0800

    [xiangxu] disable notify then build seccess

--- gate/deploy_gate.groovy
+++ gate/deploy_gate.groovy
@@ -2,7 +2,7 @@
 void call(context){
     node {
         expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
-        if (context.step.equals("Deploy to QA")){
+        if (context.step.contains("QA")){
             timeout(time:5, unit:'DAYS') {
                 input message:'Approve deployment to QA?' , ok: 'Yes'
             }
