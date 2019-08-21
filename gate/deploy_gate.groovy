777ca07c34933e3ac80d18367daa6dcc2efeb8ef
xiangxu <iasonliu@gmail.com>
2019-08-21 11:33:02 +0800

    [xiangxu] fix when

--- gate/deploy_gate.groovy
+++ gate/deploy_gate.groovy
@@ -1,14 +1,9 @@
 @BeforeStep
 void call(context){
     node {
-        step.when {
-            allOf {
-            expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
-            }
-        }
+        expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
         sh 'echo ${context}'
         if (context.step.equals("Deploy to Qa")){
-
             timeout(time:5, unit:'DAYS') {
                 input message:'Approve deployment to QA?' , ok: 'Yes'
             }
