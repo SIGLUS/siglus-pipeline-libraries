58b0a33734ccf8f25d7d9031a37d01811f767f32
xiangxu <iasonliu@gmail.com>
2019-08-21 11:37:46 +0800

    [xiangxu] fix steps error

--- gate/deploy_gate.groovy
+++ gate/deploy_gate.groovy
@@ -2,7 +2,6 @@
 void call(context){
     node {
         expression { return currentBuild.result == null || currentBuild.result == 'SUCCESS' }
-        sh 'echo ${context}'
         if (context.step.equals("Deploy to Qa")){
             timeout(time:5, unit:'DAYS') {
                 input message:'Approve deployment to QA?' , ok: 'Yes'
