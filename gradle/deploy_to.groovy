83baaadcaecfdbf407973a17f7d808d7b34eea48
qinrui <ruqin@thoughtworks.com>
2019-09-11 10:17:07 +0800

    add mutil docker-compose file

--- gradle/deploy_to.groovy
+++ gradle/deploy_to.groovy
@@ -3,14 +3,14 @@ void call(app_env){
       node {
         if (app_env.short_name == 'dev') {
             echo "******This is deploy for ${app_env.long_name}******"
-            do_deploy app_env
+            deploy app_env
         } else {   
             try {
                 timeout ( time: 30, unit: "MINUTES" ) {
                     input(message: 'Deploy this build ?')
                 }
                 echo "******This is deploy for ${app_env.long_name}******"
-                do_deploy app_env
+                deploy app_env
             } catch (err) {
               def user = err.getCauses()[0].getUser()
               if('SYSTEM' == user.toString()) { //timeout
@@ -22,15 +22,15 @@ void call(app_env){
     }     
 }
 
-def do_deploy(app_env){
+def deploy(app_env){
     withCredentials([file(credentialsId: "setting_env_${app_env.short_name}", variable: 'SETTING_ENV')]) {
       withEnv(["APP_ENV=${app_env.short_name}", "DOCKER_HOST=tcp://${app_env.hosts}:2376"]) {
         sh '''
             rm -f docker-compose*
             rm -f .env
-            rm -f settings.env
-            wget https://raw.githubusercontent.com/SIGLUS/openlmis-ref-distro/master/docker-compose.yml
-            sed -i "s#openlmis#siglusdevops#g" docker-compose.yml
+            rm -f settings.env  ${app_env.short_name}
+            wget https://raw.githubusercontent.com/SIGLUS/openlmis-ref-distro/master/docker-compose-aws-${APP_ENV}.yml
+            sed -i "s#openlmis#siglusdevops#g" docker-compose-aws-${APP_ENV}.yml
             SHORT_GIT_REVISION=$(git rev-parse --short HEAD)
             IMAGE_VERSION=${BUILD_NUMBER}-${SHORT_GIT_REVISION}
             PROJECT_NAME=${JOB_NAME%/*}
@@ -44,7 +44,7 @@ def do_deploy(app_env){
             jq -r '.[] | "curl -XPUT http://${APP_ENV}.siglus.us:8500/v1/agent/service/deregister/" + .Service.ID' > clear.sh
             chmod a+x clear.sh && ./clear.sh
             echo "Start deploy ${SERVICE_NAME} on ${APP_ENV}"
-            docker-compose -H ${DOCKER_HOST} -f docker-compose.yml -p openlmis-ref-distro up --no-deps --force-recreate -d ${SERVICE_NAME}
+            docker-compose -H ${DOCKER_HOST} -f docker-compose-aws-${APP_ENV}.yml -p openlmis-ref-distro up --no-deps --force-recreate -d ${SERVICE_NAME}
         '''
       }
     }
