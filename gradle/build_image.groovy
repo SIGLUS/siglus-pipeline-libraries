cd33f14ffd9d8ab6db3f64fa93313aa1d23d42e6
qinrui <ruqin@thoughtworks.com>
2019-09-20 10:07:36 +0800

    add branch name

--- gradle/build_image.groovy
+++ gradle/build_image.groovy
@@ -6,12 +6,12 @@ def call(){
       IMAGE_VERSION=${BUILD_NUMBER}-${SHORT_GIT_REVISION}
       PROJECT_NAME=${JOB_NAME%/*}
       IMAGE_REPO=siglusdevops/${PROJECT_NAME#*-}
-      IMAGE_NAME=${IMAGE_REPO}:${IMAGE_VERSION}
+      IMAGE_NAME=${IMAGE_REPO}-${BRANCH_NAME}:${IMAGE_VERSION}
       docker build -t ${IMAGE_NAME} .
-      docker tag ${IMAGE_NAME} ${IMAGE_REPO}:latest
+      docker tag ${IMAGE_NAME} ${IMAGE_REPO}-${BRANCH_NAME}:latest
       docker push ${IMAGE_NAME}
-      docker push ${IMAGE_REPO}:latest
-      docker rmi ${IMAGE_NAME} ${IMAGE_REPO}:latest
+      docker push ${IMAGE_REPO}-${BRANCH_NAME}:latest
+      docker rmi ${IMAGE_NAME} ${IMAGE_REPO}-${BRANCH_NAME}:latest
       echo "*************Image name is [${IMAGE_NAME}]*************"
       '''
     }
