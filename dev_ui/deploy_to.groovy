abbe40d71f0451002233177abf2aed4eddae6afc
qinrui <ruqin@thoughtworks.com>
2019-10-11 13:41:55 +0800

    fix unstable

--- dev_ui/deploy_to.groovy
+++ dev_ui/deploy_to.groovy
@@ -1,12 +1,15 @@
 void call(app_env){
   node{
-    stage("Integrated with ReferenceUI: ${app_env.long_name}") {
+    stage("Notify to build ReferenceUI: ${app_env.short_name}") {
       if (app_env.short_name == 'dev') {
         steps.build job: '../openlmis-reference-ui/master', wait: false
+        currentBuild.result = "SUCCESS"
       } else if (app_env.short_name == 'qa'){
         steps.build job: '../openlmis-reference-ui/master', wait: false
+        currentBuild.result = "SUCCESS"
       } else {
         steps.build job: '../openlmis-reference-ui/release_phase1', wait: false
+        currentBuild.result = "SUCCESS"
       }
         // steps.build job: '../openlmis-reference-ui/master', wait: false
     }
