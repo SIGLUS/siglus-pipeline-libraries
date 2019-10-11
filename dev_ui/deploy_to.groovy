fd447172db4ed217105e454ad2d3ecc878302978
qinrui <ruqin@thoughtworks.com>
2019-10-11 14:41:56 +0800

    do nothing to notify qa env

--- dev_ui/deploy_to.groovy
+++ dev_ui/deploy_to.groovy
@@ -4,7 +4,7 @@ void call(app_env){
       if (app_env.short_name == 'dev') {
         steps.build job: '../openlmis-reference-ui/master', wait: false
       } else if (app_env.short_name == 'qa'){
-        steps.build job: '../openlmis-reference-ui/master', wait: false
+        println "******  Do nothing! ******"
       } else {
         steps.build job: '../openlmis-reference-ui/release_phase1', wait: false
       }
